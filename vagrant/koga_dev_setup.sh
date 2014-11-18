d71efbbb11b90e1a3d1f3d19505c28ca3a32aacf
houdatahbaz <houda@irslogics.com>
2014-11-18 18:55:53 +0530

    changing vagrantfile

--- vagrant/koga_dev_setup.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get -y update 
-sudo apt-get -y  install build-essential libncurses5-dev openssl libssl-dev fop xsltproc unixodbc-dev curl git
-
-
-curl -o otp_src_17.3.tar.gz http://www.erlang.org/download/otp_src_17.3.tar.gz && tar xf otp_src_17.3.tar.gz && cd otp_src_17.3 && ./configure && make && sudo make install && sudo rm -rf otp_src_17.3.tar.gz 
-
-
