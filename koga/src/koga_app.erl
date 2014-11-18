dec9b82e2532aa63fc2933a2438574a38a585816
houdatahbaz <houda@irslogics.com>
2014-11-18 18:55:16 +0530

    adding koga hello world.

--- koga/src/koga_app.erl
+++ koga/src/koga_app.erl
@@ -7,13 +7,12 @@
 start(_Type, _Args) ->
     
     Dispatch = cowboy_router:compile([
-        {'_', [{"/", koga_handler, []}]}
+				      {'_', [{"/", koga_handler, []}]}
 				     ]),
-        cowboy:start_http(my_http_listener, 100, [{port, 808}],
-        [{env, [{dispatch, Dispatch}]}]
-			 ),
-        hello_erlang_sup:start_link().
-
+    cowboy:start_http(my_http_listener, 100, [{port, 8181}],
+		      [{env, [{dispatch, Dispatch}]}]
+		     ),
+    koga_sup:start_link().
 
 stop(_State) ->
 	ok.
